41c539ecfee943d707c85a204c4640ab32ab8560
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-08-12 14:55:03 -0300

    Documented the methods in the ProdutoController using Swagger.

--- Ploomes/ProdutosAPI/ProdutosAPI/Controllers/ProdutoController.cs
+++ Ploomes/ProdutosAPI/ProdutosAPI/Controllers/ProdutoController.cs
@@ -7,6 +7,7 @@ namespace ProdutosAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class ProdutoController : ControllerBase
     {
         private readonly ProdutoAPIContext _context;
@@ -16,13 +17,28 @@ namespace ProdutosAPI.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult> GetTodosProdutos() 
+        /// <summary>
+        /// Lista todos os produtos existentes na base de dados.
+        /// </summary>
+        /// <returns>Uma lista de produtos</returns>
+        /// <response code="200">Retorna todos os produtos encontrados.</response>
+        [HttpGet(Name = "GetAllProducts")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetTodosProdutos()
         {
             return Ok(await _context.Produtos.ToListAsync());
         }
 
-        [HttpGet("{id}", Name = "Get")]
+        /// <summary>
+        /// Retorna um produto, que corresponde ao ID fornecido.
+        /// </summary>
+        /// <param name="id">Número de identificação do produto.</param>
+        /// <returns>Um produto.</returns>
+        /// <response code="200">Retorna o produto requisitado.</response>
+        /// <response code="404">O produto com o ID fornecido não existe.</response>
+        [HttpGet("{id}", Name = "GetOneProduct")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> GetProduto(int id)
         {
             Produto produto = await _context.Produtos.FindAsync(id);
@@ -35,10 +51,31 @@ namespace ProdutosAPI.Controllers
             return Ok(produto);
         }
 
-        [HttpPost]
+        /// <summary>
+        /// Cadastra um produto com os dados fornecidos.
+        /// </summary>
+        /// <param name="produtoDTO">Um produto, com todos os dados necessários para o cadastro.</param>
+        /// <returns>O link para o produto.</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Produto
+        ///     {
+        ///        "nome": "Suco de Uva",
+        ///        "categoria": "Sucos",
+        ///        "preco": 5.49,
+        ///        "quantidade": 100
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Retorna o produto recém-criado.</response>
+        /// <response code="400">Caso o produto seja nulo, ou tenha dados faltando.</response>
+        [HttpPost(Name = "PostProduct")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> PostProduto(ProdutoDTO produtoDTO)
         {
-            Produto produto = new Produto() 
+            Produto produto = new Produto()
             { Nome = produtoDTO.Nome, Categoria = produtoDTO.Categoria, Preco = produtoDTO.Preco, Quantidade = produtoDTO.Quantidade };
 
             _context.Produtos.Add(produto);
@@ -46,7 +83,30 @@ namespace ProdutosAPI.Controllers
             return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
         }
 
-        [HttpPut]
+        /// <summary>
+        /// Atualiza o produto que corresponde ao ID, utilizando as novas informações fornecidas.
+        /// </summary>
+        /// <param name="id">Número de identificação do produto.</param>
+        /// <param name="produtoDTO">Um produto, com todos os dados necessários para o cadastro.</param>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /Produto
+        ///     {
+        ///        "nome": "Suco de Uva",
+        ///        "categoria": "Sucos",
+        ///        "preco": 5.49,
+        ///        "quantidade": 50
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="204">O produto foi alterado com sucesso.</response>
+        /// <response code="400">Caso o produto seja nulo, ou tenha dados faltando.</response>
+        /// <response code="404">O produto com o ID fornecido não existe.</response>
+        [HttpPut("{id}", Name = "PutProduct")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> PutProduto(int id, ProdutoDTO produtoDTO)
         {
             Produto produtoToChange = _context.Produtos.Find(id);
@@ -75,7 +135,15 @@ namespace ProdutosAPI.Controllers
             return NoContent();
         }
 
-        [HttpDelete]
+        /// <summary>
+        /// Deleta o produto que corresponde ao ID fornecido.
+        /// </summary>
+        /// <param name="id">Número de identificação do produto.</param>
+        /// <response code="204">O produto foi alterado com sucesso.</response>
+        /// <response code="404">O produto com o ID fornecido não existe.</response>
+        [HttpDelete("{id}", Name = "DeleteProduct")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> DeleteProduto(int id)
         {
             Produto produto = _context.Produtos.Find(id);
--- Ploomes/ProdutosAPI/ProdutosAPI/Program.cs
+++ Ploomes/ProdutosAPI/ProdutosAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ProdutosAPI.Context;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Produtos API",
+        Description = "Uma API do ASP.NET para consumo e cadastro de produtos.",
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddDbContext<ProdutoAPIContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProdutoAPIDatabase")));
 
 var app = builder.Build();
