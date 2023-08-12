387a0ddf9b03e7df28c8e7d94d268c14ce8a28fc
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-08-11 23:31:20 -0300

    Created the DB Context and the database.

---
+++ Ploomes/ProdutosAPI/ProdutosAPI/Context/ProdutoAPIContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutosAPI.Models;
+
+namespace ProdutosAPI.Context
+{
+    public class ProdutoAPIContext : DbContext
+    {
+        public ProdutoAPIContext(DbContextOptions<ProdutoAPIContext> options) : base(options)
+        { }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
--- Ploomes/ProdutosAPI/ProdutosAPI/Program.cs
+++ Ploomes/ProdutosAPI/ProdutosAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProdutosAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProdutoAPIContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProdutoAPIDatabase")));
 
 var app = builder.Build();
 
