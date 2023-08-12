68457586ad52fc37ae04555a1eeefd129d635252
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-08-11 23:13:55 -0300

    Created the model for 'Produto'.

---
+++ Ploomes/ProdutosAPI/ProdutosAPI/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProdutosAPI.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Categoria { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
