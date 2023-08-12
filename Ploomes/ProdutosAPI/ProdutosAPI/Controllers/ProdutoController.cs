e1155423695d1c35279e7b0abc55bae52702c3dc
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-08-12 00:48:34 -0300

    Created 'ProdutoController' with methods for GET and POST.

---
+++ Ploomes/ProdutosAPI/ProdutosAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProdutosAPI.Context;
+using ProdutosAPI.Models;
+
+namespace ProdutosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoAPIContext _context;
+
+        public ProdutoController(ProdutoAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetTodosProdutos() 
+        {
+            return Ok(await _context.Produtos.ToListAsync());
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult> GetProduto(int id)
+        {
+            Produto item = await _context.Produtos.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+        }
+    }
+}
